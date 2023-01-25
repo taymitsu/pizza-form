f0a7ee65c4a78e7498fe5a4a637beb472a23c7e2
taymitsu <tmitsumori96@gmail.com>
2023-01-25 13:02:10 -0800

    state imported, right form complete

--- src/Form.js
+++ src/Form.js
@@ -1,25 +1,52 @@
 import React from 'react';
+import { useState } from 'react';
 import './Form.css' 
 
 function Form() {
+  const [name, setName] = useState(" ")
+  const [pepperoni, setPepperoni] = useState(false)
+  const [Sausage, setSausage] = useState(false)
+  const [Peppers, setPeppers] = useState(false)
+  const [Onions, setOnions] = useState(false)
+  
   return (
     <div className="pizza-order-form">
       <h1>Pizza Order Form</h1>
       <form className="Form">
         <label>
-          <input className="Name" type="text" placeholder="Enter Name"/>
+          <input type="text" placeholder="Enter Name" value={name} onChange={e => setName(e.target.value)}/>
         </label>
         <label>
-        <input type="checkbox"/>Pepperoni
+        <input 
+          type="checkbox"
+          checked={pepperoni}
+          onChange={() => setPepperoni(!pepperoni)}
+          />
+          Pepperoni
         </label>
         <label>
-        <input type="checkbox"/>Sausage
+        <input 
+          type="checkbox"
+          checked={Sausage}
+          onChange={() => setSausage(!Sausage)}
+        />
+        Sausage
         </label>
         <label>
-        <input type="checkbox"/>Peppers
+        <input 
+          type="checkbox"
+          checked={Peppers}
+          onChange={() => setPeppers(!Peppers)}
+        />
+        Peppers
         </label>
         <label>
-        <input type="checkbox"/>Onions
+        <input 
+          type="checkbox"
+          checked={Onions}
+          onChange={() => setOnions(!Onions)}
+          />
+          Onions
         </label>
         <input className="submit" type="submit" value="Submit"/>
       </form>
